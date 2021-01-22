ae67061dcf6aeffc8761ffc9bf649070c2ca74b4
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-01-21 21:48:25 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import CatComponent from './CatComponent'
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent' 
+import MouseComponent from './MouseComponent'
 
 class App extends Component {
 	render() {
@@ -6,11 +9,19 @@ class App extends Component {
 		return (
 			<div className="App">
 				<CatComponent />
-				<GraceHopperQuoteComponent />
+				<GraceHopperQuoteComponent /> 
+				< MouseComponent/>
 				{/* one more component missing */}
 			</div>
 		);
 	}
 }
 
-export default App;
+export default App; 
+
+
+// import React, { Component } from 'react'
+// import moment from 'moment'
+// import ExampleComponent from './ExampleComponent'
+// import TestComponent from './TestComponent'
+ 
